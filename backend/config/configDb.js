da0386b63b4514bd895ae46872d3260b113c6556
tsolrven <vicaria.silvana@gmail.com>
2026-06-12 03:53:53 -0400

    Migración de TypeORM a Prisma, reconexión a PostgreSQL mediante DATABASE_URL y actualización de los archivos configDb y configEnv

--- backend/config/configDb.js
+++ backend/config/configDb.js
@@ -1,25 +1,22 @@
 'use strict';
-import { DataSource } from 'typeorm';
-import { DATABASE, DB_USERNAME, HOST, PASSWORD, DB_PORT } from './configEnv.js';
+import { PrismaClient } from '@prisma/client';
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: `${HOST}`,
-  port: DB_PORT,
-  username: `${DB_USERNAME}`,
-  password: `${PASSWORD}`,
-  database: `${DATABASE}`,
-  entities: ['src/entities/**/*.js'],
-  synchronize: true,
-  logging: false,
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
 
 export async function connectDB() {
   try {
-    await AppDataSource.initialize();
+    await prisma.$connect();
     console.log('=> Conexión exitosa a la base de datos PostgreSQL!');
   } catch (error) {
     console.error('Error al conectar con la base de datos:', error);
     process.exit(1);
   }
 }
+
+export default prisma;
